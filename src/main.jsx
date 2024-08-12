7304ebc5635a32112690db292af4bd31aedce125
Birendra <bibekbhusal400@gmail.com>
2024-08-12 12:40:57 +0545

    remove bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import store from "./Redux/store.jsx";
 
-import Store from "./Redux/Store.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={Store}>
+  <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
